569e19e0d884922d666c9044dcee2122810a9411
maxwyper <wyperma@mail.uc.edu>
2025-03-16 18:58:52 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Task 1: Declare an array of integers named dataPoints
+        int[] dataPoints = new int[100];
+
+        // Optional: Print the length of the array to verify
+        System.out.println("Array length is: " + dataPoints.length);
+    }
+}
+
+
